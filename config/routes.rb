f738fee619b3e69dab0ae9d05ed942cc08de04e1
genkim <wrc.g.miyamoto@gmail.com>
2017-06-03 10:55:32 +0900

    [modify] accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :customers
   get 'home/index'
 
-  devise_for :accounts
+  devise_for :accounts, :controllers => { :registrations => 'accounts/registrations' }
+
+  devise_scope :account do
+    get 'accounts', to: 'accounts/registrations#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
