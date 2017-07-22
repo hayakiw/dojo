1678924edfd7f551d76eb6d1be1b226b1da949e6
mamorus <mamorus@shunjiw-no-MacBook-Air-2.local>
2017-07-22 10:29:39 +0900

    [modified] kaminari pagination

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
   # GET /customers
   # GET /customers.json
   def index
-    @customers = Customer.paginate(page: params[:page], per_page: 10)
+    @customers = Customer.page(params[:page])
   end
 
   # GET /customers/1
