1769c588a294d950668b370241aaccb072db5766
mamorus <mamorus@shunjiw-no-MacBook-Air-2.local>
2017-07-23 14:25:36 +0900

    [modified] add customer test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Customer.delete_all
+
+1000.times do |i|
+	Customer.create!(
+		name: "test#{i}",
+		status: i % 2 == 0 ? true : false,
+	)
+end
